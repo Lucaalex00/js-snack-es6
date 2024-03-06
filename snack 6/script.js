7c23aec1953766c6dbff533d7891c1848b0c27cb
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-03-06 15:06:38 +0100

    add SNACK 6 BONUS commit

--- snack 6/script.js
+++ snack 6/script.js
@@ -12,7 +12,7 @@ console.log("Squadre di calcio:");
 console.table(squadreDiCalcio);
 
 //Definizione di un nuovo array VUOTO
-/* const squadreDiCalcioNomeFalli = [];
+const squadreDiCalcioNomeFalli = [];
 
 //Cicliamo forEach per individuare la singola "SQUADRA" .1
 
@@ -24,12 +24,40 @@ squadreDiCalcio.forEach(squadra => {
     console.log(squadraNome, squadraFalli);
     //Pushiamo all'interno dell'array i valori sopra riportati.
     squadreDiCalcioNomeFalli.push({ squadraNome, squadraFalli })
+    let page = document.querySelector('body');
+
+    let titlePage = document.createElement('h1')
+    titlePage.classList.add('text-center', 'red')
+    titlePage.innerHTML = `la squadra ${squadra.nome} ha subito ${squadra.falliSubiti} falli`
+    page.appendChild(titlePage)
 
 });
-console.table(squadreDiCalcioNomeFalli) */
+console.table(squadreDiCalcioNomeFalli)
+
 
 //Filtriamo in una nuova array i parametri nome e falli degli oggetti in squadreDiCalcio .2
-const listaSquadreDiCalcioFiltrata = squadreDiCalcio.map(({ nome, falliSubiti }) => ({ nome, falliSubiti }));
-console.log(listaSquadreDiCalcioFiltrata)
 
+const listaSquadreDiCalcioFiltrata = squadreDiCalcio.map(({ nome, falliSubiti }) => ({ nome, falliSubiti }))
+
+
+//BONUS
+
+var oggi = new Date();
+var giorno = oggi.getDate();
+var mese = oggi.getMonth() + 1;
+var anno = oggi.getFullYear();
+
+// Formattiamo la data 
+var dataOggi = giorno + '-' + mese + '-' + anno;
+
+
+listaSquadreDiCalcioFiltrata.forEach(squadra => {
+    let page = document.querySelector('body');
+
+    let titlePage = document.createElement('h1')
+    titlePage.classList.add('text-center', 'red')
+    titlePage.innerHTML = `la squadra ${squadra.nome} ha subito ${squadra.falliSubiti} falli fino ad oggi (${dataOggi} )`
+    page.appendChild(titlePage)
+});
+console.log(listaSquadreDiCalcioFiltrata)
 
